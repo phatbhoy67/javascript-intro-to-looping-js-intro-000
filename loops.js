0ce1d8b9bde2016671fbc6f39b25829eaed7fa2d
Chris Scanlon <chris.scanlon72@gmail.com>
2018-01-09 23:47:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,6 +20,6 @@ function maybeTrue() {
 function doWhileLoop(array){
   do {
   array.pop();
-} while (maybeTrue() || array.length === 0);
+} while (maybeTrue() && array.length > 0);
 return array;
 }
